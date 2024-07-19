26b19f61870140a75efa5e3de4294d43241cceb2
Artimus <rajbhongde4@gmail.com>
2024-07-19 10:41:40 +0530

    EDA and Model training

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT = "-e ."
-def get_requirements(file_path : str)->List[str]:
-    requiremets=[]
-    with open(file_path, 'r') as f:
-        requiremets= f.readline()
-        requiremets=[req.replace("\n","") for req in requiremets]
+HYPHEN_E_DOT = "-e ."
 
-        if HYPEN_E_DOT in requiremets:
-            requiremets.remove(HYPEN_E_DOT)
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path, 'r') as f:
+        requirements = f.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
 
 setup(
     name='ML_Project',
-    version='0.0.1',  # Version number should be a string
+    version='0.0.1',
     author='Raj',
     author_email='rajbhongade4@gmail.com',
-    description='A machine learning project',  # Adding a description
-    packages= get_requirements('requirements.txt'),
-    license='MIT',  # Including a license (adjust as needed)
+    description='A machine learning project',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    license='MIT',
 )
